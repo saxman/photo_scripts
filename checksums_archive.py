0bf6fd8b2e9977a3bc0deb113b3593e59265f4b0
Paul R. Saxman <saxman@google.com>
2016-07-04 15:47:35 -0700

    Moved file writing to after all checksums calculated

--- checksums_archive.py
+++ checksums_archive.py
@@ -8,14 +8,14 @@ import csv
 SRC_DIR = '/Volumes/Archive/Pictures'
 OUT_FILE = 'checksums_archive.tsv'
 
+checksums = []
+
+for root, subdirs, files in os.walk(SRC_DIR):
+    for file in files:
+        if file != '.DS_Store':
+            with open(os.path.join(root, file), 'rb') as _file:
+                checksums.append([root, file, hashlib.md5(_file.read()).hexdigest()])
+
 with open(OUT_FILE, 'wb') as fout:
     writer = csv.writer(fout, delimiter='\t', quotechar='\"', quoting=csv.QUOTE_MINIMAL)
-
-    for root, subdirs, files in os.walk(SRC_DIR):
-        for file_name in files:
-            file_path = os.path.join(root, file_name)
-
-            while open(file_path, 'rb') as fin:
-                checksum = hashlib.md5(fin.read()).hexdigest()
-
-            writer.writerow([root, file_name, checksum])
+    writer.writerows(checksums)
